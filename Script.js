912ac2c1d8a9621d08af1a118960e92febce63bc
DamageWasTaken <lucas.steele.sorensen@gmail.com>
2023-09-24 00:43:35 +0200

    Added navbar

--- Script.js
+++ Script.js
@@ -1,1 +1,18 @@
+console.log("Script Loaded");
 
+function selectGroup(group) {
+    if (group == "Alle"){
+        [].forEach.call(document.querySelectorAll(`.image`), function (el) {
+            el.style.visibility = 'visible';
+        });
+    } else {
+        [].forEach.call(document.querySelectorAll('.image'), function (el) {
+            el.style.visibility = 'hidden';
+        });
+        console.log(group);
+
+        [].forEach.call(document.querySelectorAll(`.${group}`), function (el) {
+            el.style.visibility = 'visible';
+        });
+    }
+}
