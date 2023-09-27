46df88d822f977aef9900ef456c36d1acfe558d4
DamageWasTaken <lucas.steele.sorensen@gmail.com>
2023-09-27 10:35:46 +0200

    Tried fixing nav issue

--- Script.js
+++ Script.js
@@ -1,11 +1,11 @@
 console.log("Script Loaded");
-[].forEach.call(document.querySelectorAll(`.container`), function (el) {
+[].forEach.call(document.querySelectorAll('.image-list'), function (el) {
     el.style.visibility = 'collapse';
 });
-
+console.log("Passed")
 function selectGroup(group) {
     if (group == "Alle"){
-        [].forEach.call(document.querySelectorAll(`.image`), function (el) {
+        [].forEach.call(document.querySelectorAll('.image'), function (el) {
             el.style.visibility = 'visible';
         });
     } else {
