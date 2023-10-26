d407b03d963aa91ae46b3fc141660a6d4111a810
DamageWasTaken <lucas.steele.sorensen@gmail.com>
2023-10-26 15:46:09 +0200

    Added full nav support

--- Script.js
+++ Script.js
@@ -1,21 +1,17 @@
 console.log("Script Loaded");
-[].forEach.call(document.querySelectorAll('.image-list'), function (el) {
-    el.style.visibility = 'collapse';
-});
-console.log("Passed")
 function selectGroup(group) {
     if (group == "Alle"){
         [].forEach.call(document.querySelectorAll('.image'), function (el) {
-            el.style.visibility = 'visible';
+            el.style.display = 'inline';
         });
     } else {
         [].forEach.call(document.querySelectorAll('.image'), function (el) {
-            el.style.visibility = 'collapse';
+            el.style.display = 'none';
         });
         console.log(group);
 
         [].forEach.call(document.querySelectorAll(`.${group}`), function (el) {
-            el.style.visibility = 'visible';
+            el.style.display = 'inline';
         });
     }
 }
