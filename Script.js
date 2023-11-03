eb0bea336cb3acadb8e20216174ad235ffd2ff6b
DamageWasTaken <lucas.steele.sorensen@gmail.com>
2023-11-03 16:52:56 +0100

    Redid how nav works + new background

--- Script.js
+++ Script.js
@@ -1,15 +1,12 @@
-console.log("Script Loaded");
 function selectGroup(group) {
     if (group == "Alle"){
-        [].forEach.call(document.querySelectorAll('.image'), function (el) {
+        [].forEach.call(document.querySelectorAll('.image-container'), function (el) {
             el.style.display = 'inline';
         });
     } else {
-        [].forEach.call(document.querySelectorAll('.image'), function (el) {
+        [].forEach.call(document.querySelectorAll('.image-container'), function (el) {
             el.style.display = 'none';
         });
-        console.log(group);
-
         [].forEach.call(document.querySelectorAll(`.${group}`), function (el) {
             el.style.display = 'inline';
         });
