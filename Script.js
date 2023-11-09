92466493a7de283dd33f029dc8a3d6260d20b3c5
DamageWasTaken <lucas.steele.sorensen@gmail.com>
2023-11-09 12:54:03 +0100

    Added generating name list based on array

--- Script.js
+++ Script.js
@@ -1,19 +1,191 @@
+// Data Converter: https://shancarter.github.io/mr-data-converter/
+
+displayName = [
+{nunber:133,name:"Adelina Høst Johnsen",img:"viggo-billeder/A/Adelina.jpg"},
+{nunber:7,name:"Albert Damsgaard Grønnerup",img:"viggo-billeder/A/Albert.jpg"},
+{nunber:81,name:"Alberte Holmgaard Grønfeldt",img:"viggo-billeder/A/Alberte.jpg"},
+{nunber:14,name:"Alexander Damgård",img:"viggo-billeder/A/Alexander.jpg"},
+{nunber:82,name:"Alma Frølich Hougaard",img:"viggo-billeder/A/Alma.jpg"},
+{nunber:73,name:"Amalie Kortfiz Nielsen",img:"viggo-billeder/A/Amalie.jpg"},
+{nunber:97,name:"Andrea Bjørnskov Bergholt",img:"viggo-billeder/A/Andrea B.jpg"},
+{nunber:103,name:"Andrea Nørup Andersen",img:"viggo-billeder/A/Andrea N.jpg"},
+{nunber:139,name:"Angelina Christensen Lyberth",img:"viggo-billeder/A/Angelina.jpg"},
+{nunber:41,name:"Anne Wilstrup Holst",img:"viggo-billeder/A/Anne.jpg"},
+{nunber:44,name:"Anton Bissø Stausholm",img:"viggo-billeder/A/Anton B.jpg"},
+{nunber:65,name:"Anton Haukrog Møller",img:"viggo-billeder/A/Anton H M.jpg"},
+{nunber:134,name:"Anton Hermansen",img:"viggo-billeder/A/Anton H.jpg"},
+{nunber:104,name:"Astrid Astrup Laursen",img:"viggo-billeder/A/Astrid A.jpg"},
+{nunber:6,name:"Astrid Marie Kirkegaard",img:"viggo-billeder/A/Astrid M.jpg"},
+{nunber:15,name:"Axel Christian Skovgaard Nielsen",img:"viggo-billeder/A/Axel.jpg"},
+{nunber:131,name:"Baylee Halfdan Søgård Fink",img:"viggo-billeder/B/Baylee.jpg"},
+{nunber:27,name:"Benjamin Samuel Nilsen Nicolajsen",img:"viggo-billeder/B/Benjamin.jpg"},
+{nunber:98,name:"Camilla Lehmann Jessen",img:"viggo-billeder/C/Camilla.jpg"},
+{nunber:127,name:"Camille Staberg Simonsen",img:"viggo-billeder/C/Camille.jpg"},
+{nunber:52,name:"Carolina Aagaard",img:"viggo-billeder/C/Carolina.jpg"},
+{nunber:47,name:"Caroline Flintholm Juelsgaard",img:"viggo-billeder/C/Caroline F.jpg"},
+{nunber:45,name:"Caroline Sax Riggelsen",img:"viggo-billeder/C/Caroline S.jpg"},
+{nunber:119,name:"Cecilia Manfeld-Ørtoft",img:"viggo-billeder/C/Cecilia.svg"},
+{nunber:60,name:"Cecilie Damsgaard Paulsen",img:"viggo-billeder/C/Cecilie D.jpg"},
+{nunber:57,name:"Cecilie Jamilya Stolborg Mendes",img:"viggo-billeder/C/Cecilie J.jpg"},
+{nunber:108,name:"Cecilie Margrethe Christiansen",img:"viggo-billeder/C/Cecilie M.jpg"},
+{nunber:77,name:"Dicte Johanne Schultz",img:"viggo-billeder/D/Dicte.jpg"},
+{nunber:136,name:"Ella Zwergius Moreno",img:"viggo-billeder/E/Ella.jpg"},
+{nunber:3,name:"Emil Støttrup",img:"viggo-billeder/E/Emil.jpg"},
+{nunber:113,name:"Emilie Christensen",img:"viggo-billeder/E/Emilie.jpg"},
+{nunber:126,name:"Emily Aviaya Zimmermann",img:"viggo-billeder/E/Emily.svg"},
+{nunber:116,name:"Emma Lindberg Jensen",img:"viggo-billeder/E/Emma.jpg"},
+{nunber:75,name:"Esben Petersen",img:"viggo-billeder/E/Esben.jpg"},
+{nunber:21,name:"Eva Ragauge Muntenjon",img:"viggo-billeder/E/Eva.jpg"},
+{nunber:96,name:"Evelina Spelmane",img:"viggo-billeder/E/Evelina.jpg"},
+{nunber:78,name:"Faye Randris Rowlands",img:"viggo-billeder/F/Faye.jpg"},
+{nunber:8,name:"Frederik Haugaard Brunsgaard",img:"viggo-billeder/F/Frederik H B.jpg"},
+{nunber:18,name:"Frederik Højmark Pedersen",img:"viggo-billeder/F/Frederik H P.jpg"},
+{nunber:111,name:"Frederik William Dirksen",img:"viggo-billeder/F/Frederik W.jpg"},
+{nunber:110,name:"Freja Michelle Andersen",img:"viggo-billeder/F/Freja.jpg"},
+{nunber:61,name:"Frida Friis Thomsen",img:"viggo-billeder/F/Frida F.jpg"},
+{nunber:138,name:"Frida Peters Lassen",img:"viggo-billeder/F/Frida P.jpg"},
+{nunber:64,name:"Frida Søgaard Thomsen",img:"viggo-billeder/F/Frida S.jpg"},
+{nunber:63,name:"Gry Lindholm Tollgaard",img:"viggo-billeder/G/Gry.jpg"},
+{nunber:20,name:"Gunna Rump Vejlø",img:"viggo-billeder/G/Gunna.jpg"},
+{nunber:99,name:"Halfdan Jacobsen Kirkegaard",img:"viggo-billeder/H/Halfdan.jpg"},
+{nunber:36,name:"Hanibal Ingeberg",img:"viggo-billeder/H/Hanibal.jpg"},
+{nunber:67,name:"Harald Brøcker",img:"viggo-billeder/H/Harald.jpg"},
+{nunber:56,name:"Hjalte Wittek Sørensen",img:"viggo-billeder/H/Hjalte.jpg"},
+{nunber:34,name:"Hugo Qvist Tøpholm",img:"viggo-billeder/H/Hugo.jpg"},
+{nunber:92,name:"Ingeborg Brunn Hinrichsen",img:"viggo-billeder/I/Ingeborg.jpg"},
+{nunber:112,name:"Isabella Kirstine Neuberg Rasmussen",img:"viggo-billeder/I/Isabella.jpg"},
+{nunber:76,name:"Jens Møller Martin",img:"viggo-billeder/J/Jens.jpg"},
+{nunber:120,name:"Jeppe Holm Jensen",img:"viggo-billeder/J/Jeppe.jpg"},
+{nunber:32,name:"Johan Lading Katballe",img:"viggo-billeder/J/Johan K.jpg"},
+{nunber:43,name:"Johan Stenkil Lajer",img:"viggo-billeder/J/Johan S.jpg"},
+{nunber:16,name:"Jonas Rotne",img:"viggo-billeder/J/Jonas R.jpg"},
+{nunber:128,name:"Jonas Staghøj Markussen",img:"viggo-billeder/J/Jonas S.jpg"},
+{nunber:88,name:"Julie Christensen Venø",img:"viggo-billeder/J/Julie C.jpg"},
+{nunber:54,name:"Julie Lykke Lolck",img:"viggo-billeder/J/Julie L.jpg"},
+{nunber:26,name:"Justin Vesterlund",img:"viggo-billeder/J/Justin.jpg"},
+{nunber:1,name:"Karla Kobberø",img:"viggo-billeder/K/Karla K.jpg"},
+{nunber:53,name:"Karla Weimar Schousen",img:"viggo-billeder/K/Karla W.jpg"},
+{nunber:69,name:"Karoline Pagaard Christensen",img:"viggo-billeder/K/Karoline.jpg"},
+{nunber:93,name:"Katja Nygaard-Larsen",img:"viggo-billeder/K/Katja.jpg"},
+{nunber:94,name:"Katrine Brylle",img:"viggo-billeder/K/Katrine.jpg"},
+{nunber:124,name:"Kirstine Ammiztbøll Christensen",img:"viggo-billeder/K/Kirstine.jpg"},
+{nunber:70,name:"Knud Erik Mai Ding",img:"viggo-billeder/K/Knud.jpg"},
+{nunber:91,name:"Kristoffer Rehhoff-Nør",img:"viggo-billeder/K/Kristoffer.jpg"},
+{nunber:68,name:"Laura Vinther Jepsen",img:"viggo-billeder/L/Laura.jpg"},
+{nunber:72,name:"Lina Høft Homilius",img:"viggo-billeder/L/Lina.jpg"},
+{nunber:105,name:"Liva Marie Mikel Schjelde",img:"viggo-billeder/L/Liva.jpg"},
+{nunber:38,name:"Lucas Steele Sørensen",img:"viggo-billeder/L/Lucas S.jpg"},
+{nunber:37,name:"Lucas Wittrup Jensen",img:"viggo-billeder/L/Lucas W.jpg"},
+{nunber:101,name:"Lykke Pagaard",img:"viggo-billeder/L/Lykke.jpg"},
+{nunber:58,name:"Magnus Beyer Bach Mortensen",img:"viggo-billeder/M/Magnus.jpg"},
+{nunber:84,name:"Malthe Melvild Greve",img:"viggo-billeder/M/Malte M.jpg"},
+{nunber:79,name:"Malthe Drud Vester Palle",img:"viggo-billeder/M/Malthe D.jpg"},
+{nunber:135,name:"Malthe Gustavussen",img:"viggo-billeder/M/Malthe G.jpg"},
+{nunber:22,name:"Marcus Thorup",img:"viggo-billeder/M/Marcus.jpg"},
+{nunber:46,name:"Maria Løvlund Mandsberg",img:"viggo-billeder/M/Maria L.jpg"},
+{nunber:83,name:"Maria West Jørgensen",img:"viggo-billeder/M/Maria W.jpg"},
+{nunber:117,name:"Marie Hebsgaard Offersen",img:"viggo-billeder/M/Marie H.jpg"},
+{nunber:106,name:"Marius Ehlert Degn Larsen",img:"viggo-billeder/M/Marius E.jpg"},
+{nunber:87,name:"Marius Kjeld Finnerup",img:"viggo-billeder/M/Marius KF.jpg"},
+{nunber:107,name:"Marius Lykke Hansen",img:"viggo-billeder/M/Marius LH.jpg"},
+{nunber:40,name:"Mary Fischer Sloth",img:"viggo-billeder/M/Mary.jpg"},
+{nunber:66,name:"Mathias Bach Sølvberg Dolleris",img:"viggo-billeder/M/Mathias.jpg"},
+{nunber:85,name:"Mathilde Dræborg Evind",img:"viggo-billeder/M/Mathilde.jpg"},
+{nunber:130,name:"Merle Toft",img:"viggo-billeder/M/Merle.jpg"},
+{nunber:10,name:"Mette Kulby Rick",img:"viggo-billeder/M/Mette.jpg"},
+{nunber:71,name:"Mille Sandholdt",img:"viggo-billeder/M/Mille S.jpg"},
+{nunber:25,name:"Mille Schultz Bach",img:"viggo-billeder/M/Mille SB.jpg"},
+{nunber:129,name:"Mille Sophia Damsø",img:"viggo-billeder/M/Mille SD.jpg"},
+{nunber:86,name:"Mina Skjoldby Foss",img:"viggo-billeder/M/Mina.jpg"},
+{nunber:132,name:"Nagi Nisioka",img:"viggo-billeder/N/Nagi.jpg"},
+{nunber:114,name:"Nanna Lerche Freudendal",img:"viggo-billeder/N/Nanna.jpg"},
+{nunber:123,name:"Nellie Berg Pedersen",img:"viggo-billeder/N/Nellie.jpg"},
+{nunber:42,name:"Noah Bue Pilgaard",img:"viggo-billeder/N/Noah B.jpg"},
+{nunber:12,name:"Noah Grøndahl Lassen",img:"viggo-billeder/N/Noah G.jpg"},
+{nunber:24,name:"Noah Aarhus Petersen",img:"viggo-billeder/N/Noah A.jpg"},
+{nunber:102,name:"Ole Carl Risak Schou",img:"viggo-billeder/O/Lego.jpg"},
+{nunber:9,name:"Oliver Gaba Lylover",img:"viggo-billeder/O/Oliver G.jpg"},
+{nunber:51,name:"Oliver Scott Walters",img:"viggo-billeder/O/Oliver S.jpg"},
+{nunber:62,name:"Peter Bille Røhling",img:"viggo-billeder/P/Peter.jpg"},
+{nunber:35,name:"Philip Hilstrøm Sørensen",img:"viggo-billeder/P/Philip.jpg"},
+{nunber:80,name:"Rasmus Benjamin Jørgensen",img:"viggo-billeder/R/Rasmus B.jpg"},
+{nunber:31,name:"Rasmus Kirkeskov Hansen",img:"viggo-billeder/R/Rasmus KH.jpg"},
+{nunber:11,name:"Rasmus Kyhn Boisen",img:"viggo-billeder/R/Rasmus K.jpg"},
+{nunber:89,name:"Rune Horsbøll Møller",img:"viggo-billeder/R/Rune.jpg"},
+{nunber:90,name:"Sara Møller Risdahl Knudsen",img:"viggo-billeder/S/Sara.jpg"},
+{nunber:4,name:"Sebastian Bisgaard",img:"viggo-billeder/S/Sebastian B.jpg"},
+{nunber:122,name:"Sebastian Skiby Esbjerg",img:"viggo-billeder/S/Sebastian S.jpg"},
+{nunber:115,name:"Sebastian Valdemar Sax Sielemann",img:"viggo-billeder/S/Sebastian V.jpg"},
+{nunber:50,name:"Silje Andrea Palmelund Lindemann",img:"viggo-billeder/S/Silje.jpg"},
+{nunber:30,name:"Simon Wonsild Krogsgaard Andersen",img:"viggo-billeder/S/Simon.jpg"},
+{nunber:95,name:"Sofie Fynbo Christoffersen",img:"viggo-billeder/S/Sofie.jpg"},
+{nunber:49,name:"Sofus Czeloth Steenskov",img:"viggo-billeder/S/Sofus.jpg"},
+{nunber:137,name:"Stine Louise Clausen",img:"viggo-billeder/S/Stine.jpg"},
+{nunber:5,name:"Svend Nilsen Korsholm",img:"viggo-billeder/S/Svend.jpg"},
+{nunber:109,name:"Tea Bendixen",img:"viggo-billeder/T/Tea.jpg"},
+{nunber:17,name:"Thea Dalsgaard Kallsø",img:"viggo-billeder/T/Thea.jpg"},
+{nunber:29,name:"Tobias Grønskov Hansen",img:"viggo-billeder/T/Tobias G.jpg"},
+{nunber:28,name:"Tobias Høgh Veis",img:"viggo-billeder/T/Tobias H.jpg"},
+{nunber:55,name:"Torbjørn Hornemann Nielsen",img:"viggo-billeder/T/Torbjørn.jpg"},
+{nunber:19,name:"Tristian Alexander Hald",img:"viggo-billeder/T/Tristian.jpg"},
+{nunber:33,name:"Troles Kragerup Lundin",img:"viggo-billeder/T/Troles.jpg"},
+{nunber:59,name:"Tue Storm Mathiasen",img:"viggo-billeder/T/Tue.jpg"},
+{nunber:2,name:"Victor Albert Christensen",img:"viggo-billeder/V/Victor.jpg"},
+{nunber:121,name:"Victoria Roskvist",img:"viggo-billeder/V/Victoria.jpg"},
+{nunber:48,name:"Vigga Burmeister Clausen",img:"viggo-billeder/V/Vigga.jpg"},
+{nunber:39,name:"Vilma Torsrup Melgaard",img:"viggo-billeder/V/Vilma.jpg"},
+{nunber:13,name:"Zenia Karen Van Der Plas",img:"viggo-billeder/Z/Zenia.jpg"}
+]
+
 function selectGroup(group) {
     if (group == "Alle"){
         [].forEach.call(document.querySelectorAll('.image-container'), function (el) {
-            el.style.display = 'inline';
+            el.style.display = 'flex';
         });
     } else {
         [].forEach.call(document.querySelectorAll('.image-container'), function (el) {
             el.style.display = 'none';
         });
         [].forEach.call(document.querySelectorAll(`.${group}`), function (el) {
-            el.style.display = 'inline';
+            el.style.display = 'flex';
         });
     }
 }
 
-function openPopup(person) {
+window.onload = () => {
+    appendData(displayName)
+
+    function appendData(displayName) {
+        window.data = displayName
+        var mainContainer = document.getElementById("container");
+        for (var i = 0; i < displayName.length; i++) {
+            mainContainer.insertAdjacentHTML("beforeend",'<div class="image-container ' + displayName[i].name.charAt(0) + '" onclick="openPopup('+ i +')">' + '<img src="' + displayName[i].img + '" class="image"> <p class="name-text">' + displayName[i].name + '</p> </div>',);
+        }
+        
+    }
+}
+
+function showinfo(i) {
+  document.getElementById("configurationdetails").innerHTML = displayName[i].name + " " + displayName[i].number;
+} 
+
+for (var x = 0; x < displayName.Length; x++) {
+    var students = document.getElementsByClassName("image-container");
+    students.onclick = openPopup('x');
+    students.setAttribute("onclick","openPopup('+ i +');");
+    console.log(x)
+}
+
+function openPopup(i) {
     var popup = document.getElementById("popup");
     popup.classList.toggle("show");
-  }
+    popup.classList.toggle("blur-bg");
+
+    document.getElementById("replaceableText").innerHTML = displayName[i].name;
+}
+
+function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.classList.toggle("show");
+    popup.classList.toggle("blur-bg");
+}
